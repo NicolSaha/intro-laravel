83c57ca4f2e9c5b4dc03a7e2c819e6806cf4ef16
“[Nicol <[nicol.saha@gmail.com]>
2020-10-14 19:49:22 +0200

    Add migration file

---
+++ database/migrations/2020_10_14_152649_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id('id');
+            $table->timestamps();
+            $table->string('FirstName', 100);
+            $table->string('LastName', 100);
+            $table->integer('StarRating');
+            $table->text('ReviewText');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
